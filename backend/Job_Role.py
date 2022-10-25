a223bd15f6e5f978274b51b119869b46190ae40a
Yee Sen :) <ystan98@hotmail.com>
2022-10-25 23:13:29 +0800

    changed status number

--- backend/Job_Role.py
+++ backend/Job_Role.py
@@ -33,7 +33,7 @@ class Job_Role(db.Model):
 def create_job_role():
     data = request.get_json()
     new_job_role = Job_Role(
-        data['job_role_name'], data['job_role_desc'], 1)
+        data['job_role_name'], data['job_role_desc'], 0)
     # check is existing role is there
     jobRoles = Job_Role.query.all()
     if jobRoles != None:
