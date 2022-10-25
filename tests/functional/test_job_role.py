6eb83204635347fc2f5dadf2c8b5c5ce0e809ed6
ndp1998 <vanguardlover537@gmail.com>
2022-10-25 23:14:45 +0800

    new changes

--- tests/functional/test_job_role.py
+++ tests/functional/test_job_role.py
@@ -5,11 +5,10 @@ from backend.app import app, db
 from backend.Job_Role import Job_Role
 
 class TestApp(flask_testing.TestCase):
-    app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+mysqlconnector://root@localhost:3306/LJPS_DB"
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
-    app.config['TESTING'] = True
-
     def create_app(self):
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {}
+        app.config['TESTING'] = True
         return app
 
     def setUp(self):
@@ -23,19 +22,82 @@ class TestJobRole(TestApp):
     def test_create_job_role(self):
         request_body = {
             "job_role_name": "Human Resource",
-            "job_role_desc": "Human Resource is a job role",
-            "job_role_status": 1
+            "job_role_desc": "HR's job role",
         }
-        with app.app_context():
-            response = self.client.post("/createJobRole",
+        response = self.client.post('/createJobRole', 
                                     data=json.dumps(request_body),
                                     content_type='application/json')
-            assert response.json['code'] == 201
-            assert response.json['message'] == "Job Role successfully created"
-            assert response.json['data']['job_role_name'] == "Human Resource"
-            assert response.json['data']['job_role_desc'] == "Human Resource is a job role"
-            assert response.json['data']['job_role_status'] == 1
-    #deletion
+        self.assertEqual(response.json, {
+            "code" : 201,
+            "data": {
+                "job_role_desc": "HR's job role",
+                "job_role_id": 1,
+                "job_role_name": "Human Resource",
+                "job_role_status": 1
+            },
+            "message": "Job Role successfully created",
+        })
+
+    def test_get_job_role(self):
+        job_role = Job_Role(job_role_name="Human Resource", job_role_desc="HR's job role", job_role_status=1)
+        db.session.add(job_role)
+        db.session.commit()
+
+        response = self.client.get('/getAllJobRole')
+        self.assertEqual(response.json, {
+            "code" : 200,
+            "data": [
+                {
+                    "job_role_desc": "HR's job role",
+                    "job_role_id": 1,
+                    "job_role_name": "Human Resource",
+                    "job_role_status": 1
+                },
+                {
+                    "job_role_desc": "HR's job role",
+                    "job_role_id": 2,
+                    "job_role_name": "Human Resource",
+                    "job_role_status": 1
+                }
+            ]})
+
+
+    # def test_get_specific_job_role_by_id(self):
+    #     # job_role = Job_Role("Human Resource", "HR's job role", 1)
+    #     # db.session.add(job_role)
+    #     # db.session.commit()
+    #     response = self.client.get('/getSpecificJobRole/1')
+    #     self.assertEqual(response.json['data'][0]['job_role_name'], "Human Resource")
+    #     self.assertEqual(response.json['data'][0]['job_role_desc'], "HR's job role")
+    #     self.assertEqual(response.json['data'][0]['job_role_status'], 1)
+
+    # def test_update_job_role(self):
+    #     # job_role = Job_Role("Human Resource", "HR's job role", 1)
+    #     # db.session.add(job_role)
+    #     # db.session.commit()
+
+    #     request_body = {
+    #         "job_role_name": "Software Development",
+    #         "job_role_desc": "SD's job role",
+    #         "job_role_status": 0
+    #     }
         
+    #     response = self.client.put('/updateJobRole/1', 
+    #                                 data=json.dumps(request_body),
+    #                                 content_type='application/json') 
+    #     self.assertEqual(response.json["data"]["job_role_name"], "Software Development")                
+    #     self.assertEqual(response.json["data"]["job_role_desc"], "SD's job role")
+    
+    # def test_delete_job_role(self):
+    #     job_role = Job_Role("Human Resource", "HR's job role", 1)
+    #     db.session.add(job_role)
+    #     db.session.commit()
+        
+    #     response = self.client.delete('/deleteRole/1')
+    #     self.assertEqual(response.json["code"] , 200)
+    #     self.assertEqual(response.json["message"] , "Job removed successfully")
+
+if __name__ == '__main__':
+    unittest.main()
     
 
