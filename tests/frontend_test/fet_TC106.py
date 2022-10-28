5d10ff3c248324c89565b246d8bb55e03dfa147c
Funnymark55 <87269785+heiEzekiel@users.noreply.github.com>
2022-10-28 15:38:23 +0800

    Added fe automated test cases

--- tests/frontend_test/fet_TC106.py
+++ tests/frontend_test/fet_TC106.py
@@ -1,42 +1,42 @@
 #Just run the script to test and rmb to pip install libs
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-# from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from url import url
 
-
-url = "http://127.0.0.1:5500/frontend/hr/view_course.html"
+url = url + "frontend/hr/create_role.html"
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 browser.get(url)
-element_present = EC.presence_of_element_located((By.ID, 'table'))
+element_present = EC.presence_of_element_located((By.ID, 'title'))
 WebDriverWait(browser, 2).until(element_present)
+
 print("\n\n===== Results =====")
 try:
-    thead = browser.find_element(By.ID, "table_head")
-    tbody = browser.find_element(By.ID, "table_body")
-    assign_button = browser.find_element(By.ID, "COR001")
-    assign_button.click()
-    browser.get(browser.current_url)
-    time.sleep(1)
-    element_present = EC.presence_of_element_located((By.ID, 'table'))
+    role_name_box = browser.find_element(By.ID, "role_name")
+    role_desc_box = browser.find_element(By.ID, "role_desc")
+    create_button = browser.find_element(By.ID, "btn")
+    element_present = EC.presence_of_element_located((By.ID, 'btn'))
     WebDriverWait(browser, 2).until(element_present)
-    add_button = browser.find_element(By.ID, "Machine learning")
-    add_button.click()
+    if role_name_box and role_desc_box and create_button:
+        role_name_box.send_keys("helloworldhelloworldhelloworldhelloworldhelloworldd")
+        role_desc_box.send_keys("this is valid description")
+    create_button.click()
+    
     alert = WebDriverWait(browser, 5).until(EC.alert_is_present())
     alert_text = alert.text
-    if alert_text == "Added skill to course successfully":
+    
+    if "maximum of 50 characters" in alert_text:
         browser.switch_to.alert.accept()
         print(f"\n{url}\nTest passed!")
     else:
         print(f"\n{url}\nTest failed!")
-        print('Need remove before add')
+        print('Error occured')
 except:
     print(f"\n{url}\nTest failed!")
 browser.quit()
--- tests/frontend_test/fet_TC114.py
+++ tests/frontend_test/fet_TC114.py
@@ -1,42 +1,42 @@
 #Just run the script to test and rmb to pip install libs
-import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-# from selenium.common.exceptions import TimeoutException, NoSuchElementException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from url import url
 
-
-url = "http://127.0.0.1:5500/frontend/hr/view_course.html"
+url = url + "frontend/hr/skills_form.html"
 chrome_options = Options()
 chrome_options.add_argument("--headless")
 browser = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 browser.get(url)
-element_present = EC.presence_of_element_located((By.ID, 'table'))
+element_present = EC.presence_of_element_located((By.ID, 'title'))
 WebDriverWait(browser, 2).until(element_present)
+
 print("\n\n===== Results =====")
 try:
-    thead = browser.find_element(By.ID, "table_head")
-    tbody = browser.find_element(By.ID, "table_body")
-    assign_button = browser.find_element(By.ID, "COR001")
-    assign_button.click()
-    browser.get(browser.current_url)
-    time.sleep(1)
-    element_present = EC.presence_of_element_located((By.ID, 'table'))
+    skill_name_box = browser.find_element(By.ID, "skillName")
+    skill_desc_box = browser.find_element(By.ID, "skillDescription")
+    create_button = browser.find_element(By.ID, "create_btn")
+    element_present = EC.presence_of_element_located((By.ID, 'create_btn'))
     WebDriverWait(browser, 2).until(element_present)
-    add_button = browser.find_element(By.ID, "Machine learning")
-    add_button.click()
+    if skill_name_box and skill_desc_box and create_button:
+        skill_name_box.send_keys("Project Management")
+        skill_desc_box.send_keys("")
+    create_button.click()
+    
     alert = WebDriverWait(browser, 5).until(EC.alert_is_present())
     alert_text = alert.text
-    if alert_text == "Added skill to course successfully":
+    
+    if "Fill up all inputs" in alert_text:
         browser.switch_to.alert.accept()
         print(f"\n{url}\nTest passed!")
     else:
         print(f"\n{url}\nTest failed!")
-        print('Need remove before add')
+        print('Error occured')
 except:
     print(f"\n{url}\nTest failed!")
 browser.quit()
