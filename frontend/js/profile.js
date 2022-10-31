1c67858104c848a11a9d6e9f7c30e228fbc8cd2e
ystan98 <66168700+ystan98@users.noreply.github.com>
2022-10-31 10:31:39 +0800

    edited profile.js

--- frontend/js/profile.js
+++ frontend/js/profile.js
@@ -3,6 +3,7 @@ var getJourney = 'getJourney';
 var deleteJourney = 'deleteJourney';
 var getCourse = 'getCourseReg'
 var getSkillStaff = 'getSkillStaff'
+var getRole = 'getAllJobRole'
 staff_id = 140001 // dynamic later
 
 async function getCoursesRegistration(id) {
@@ -68,47 +69,14 @@ async function getLJ(staff_id) {
     .then(response => response.json())
     .then(data => {
 
-      if (data.code ==404){
-        document.getElementById("no_lj").innerHTML=" <span class=\"text-danger\">No ongoing Learning Journey </span>"
+      if (data.code == 404) {
+        document.getElementById("no_lj").innerHTML = " <span class=\"text-danger\">No ongoing Learning Journey </span>"
+      } else {
+
+
+        result = JSON.parse(JSON.stringify(data.data))
+        getLJstatus(result)
       }
-
-      else{
-
-     
-      result = JSON.parse(JSON.stringify(data.data))
-      var count=0
-      var lj_c = 0
-      var message_str = ""
-      
-  
-
-      for (lj of result) {
-
-        if (lj.journey_status == 'Completed') {
-          count+= 1
-        } else {
-          lj_c+=1
-          message_str += `                      
-                <div class="card mt-3">
-<div class="card-body">
-  <h5 class="card-title fs-6 fw-bold">${lj.journey_name}    
-  <span  onClick="removeJourney(${lj.journey_id})" class="mt-1 mx-1 btn btn-danger text-white float-end" style="font-size:10px;" >
-  Remove learning journey
-  </span>
-  <span onClick="location.href='./update_lj.html?id=${lj.journey_id}&name=${lj.journey_name}'" class="mt-1 btn btn-primary text-white float-end" style="font-size:10px;"> View Learning Journey </span>
-  
-
-</h5>
-
-</div>
-</div>`
-        }
-
-
-      }
-      document.getElementById("countlj").innerText = lj_c
-      document.getElementById("completedlj").innerText = count
-      document.getElementById("lj").innerHTML += message_str }
     })
     .catch(error => {
       // Errors when calling the service; such as network error, 
@@ -116,6 +84,83 @@ async function getLJ(staff_id) {
       console.log(error);
     });
 }
+
+async function getLJstatus(journey) {
+  var count = 0
+  var lj_c = 0
+  var message_str = ""
+
+  const response =
+    await fetch(`${backendUrl}/${getRole}`)
+    .then(response => response.json())
+    .then(data => {
+      result = JSON.parse(JSON.stringify(data.data))
+      console.log(result)
+      var count = 0
+      var lj_c = 0
+      var message_str = ""
+
+
+
+
+
+      for (lj of journey) {
+
+        if (lj.journey_status == 'Completed') {
+          count += 1
+        } else {
+          lj_c += 1
+          for (r of result) {
+            if(r.job_role_id==lj.j_fk_job_role_id){
+            console.log(r.job_role_id)
+            if (r.job_role_status == 0) { //active
+              message_str += `                      
+              <div class="card mt-3">
+<div class="card-body">
+<h5 class="card-title fs-6 fw-bold">${lj.journey_name}    
+<span  onClick="removeJourney(${lj.journey_id})" class="mt-1 mx-1 btn btn-danger text-white float-end" style="font-size:10px;" >
+Remove learning journey
+</span>
+<span onClick="location.href='./update_lj.html?id=${lj.journey_id}&name=${lj.journey_name}'" class="mt-1 btn btn-primary text-white float-end" style="font-size:10px;"> View Learning Journey </span>
+
+
+</h5>
+
+</div>
+</div>`
+            } else {
+              message_str += `                      
+              <div class="card mt-3">
+<div class="card-body">
+<h5 class="card-title fs-6 fw-bold">${lj.journey_name} <span class="text-danger">(Inactive)</span>   
+<span  onClick="removeJourney(${lj.journey_id})" class="mt-1 mx-1 btn btn-danger text-white float-end" style="font-size:10px;" >
+Remove learning journey
+</span>
+<span onClick="location.href='./update_lj.html?id=${lj.journey_id}&name=${lj.journey_name}'" class="mt-1 btn btn-primary text-white float-end" style="font-size:10px;"> View Learning Journey </span>
+
+
+</h5>
+
+</div>
+</div>`
+            }}
+          }
+
+        }
+
+
+      }
+      document.getElementById("countlj").innerText = lj_c
+      document.getElementById("completedlj").innerText = count
+      document.getElementById("lj").innerHTML += message_str
+    })
+    .catch(error => {
+      // Errors when calling the service; such as network error, 
+      // service offline, etc
+      console.log(error);
+    });
+}
+
 
 
 getLJ(staff_id);
