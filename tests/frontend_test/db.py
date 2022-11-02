4ac48ad47a416c6bd97dcad9af0f41aee6968b63
Funnymark55 <87269785+heiEzekiel@users.noreply.github.com>
2022-11-02 17:28:14 +0800

    Update db.py

--- tests/frontend_test/db.py
+++ tests/frontend_test/db.py
@@ -1,2 +1,3 @@
 import sqlalchemy
 engine = sqlalchemy.create_engine("mysql+mysqlconnector://root@localhost:3306/LJPS_DB")
+#engine = sqlalchemy.create_engine("mysql+mysqlconnector://admin:SoftwareProject@spm.czdb9a0r4ea9.ap-southeast-1.rds.amazonaws.com:3306/LJPS_DB")
